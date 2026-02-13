bc8e6cffcbaaa5503b56137f2558a8fcbc0fdaa5
Zhihao Zhang <2961112Z@student.gla.ac.uk>
2026-02-13 01:22:04 +0000

    Chapter 5

--- rango/forms.py
+++ rango/forms.py
@@ -18,10 +18,8 @@ class PageForm(forms.ModelForm):
         max_length=128,
         help_text="Please enter the title of the page."
     )
-    url = forms.URLField(
-        max_length=200,
-        help_text="Please enter the URL of the page."
-    )
+    url = forms.CharField(max_length=200, help_text="Please enter the URL of the page.")
+
     views = forms.IntegerField(
         widget=forms.HiddenInput(),
         initial=0
@@ -30,4 +28,14 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://') and not url.startswith('https://'):
+            cleaned_data['url'] = 'http://' + url
+
+        return cleaned_data
+
 
