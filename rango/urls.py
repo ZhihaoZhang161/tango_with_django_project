eafeab69810b55bd60db8edce9b4f2e40c76028e
Zhihao Zhang <2961112Z@student.gla.ac.uk>
2026-02-13 01:04:44 +0000

    Chapter 5

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'rango'
 
 urlpatterns = [
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('add_category/', views.add_category, name='add_category'),
     path('', views.index, name='index'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
